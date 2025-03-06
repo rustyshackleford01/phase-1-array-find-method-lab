9cbbe8f0e9c4ed04c3a6c283fdcc383cfe345adc
Samantha Webb <samantha.webb2436@gmail.com>
2025-03-05 22:19:38 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+
+
+//
+
+function superbowlWin(record) {
+    const winningYear = record.find(entry => entry.result === "W");
+if (winningYear) {
+    return winningYear.year;
+} else {
+    return undefined;
+}
+};
+
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" }
+];
+
+console.log(superbowlWin(record))
